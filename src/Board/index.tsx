34b462c2403734d43af6816fd2fa87d25c9cff0f
gabin.bloquet <gabin.bloquet@decathlon.com>
2021-02-28 17:00:09 +0100

    feat: add homepage :style:

---
+++ src/Board/index.tsx
@@ -0,0 +1,11 @@
+import './Board.css';
+
+const Board = () => {
+  return (
+    <div className="board">
+      <h1>Welcome in our wonderful TIC TAC TOE application !</h1>
+    </div>
+  );
+}
+
+export default Board;
